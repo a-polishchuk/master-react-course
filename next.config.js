7d1949e416eba60138bb166bcb7e8e5f1ba6e1da
Andrii Polishchuk <polishchuk91@gmail.com>
2025-01-04 17:42:42 +0200

    configuring gh-pages deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
+
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
     },
-    basePath: '/mastering-react-landing',
-    assetPrefix: '/mastering-react-landing',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 };
 
 module.exports = nextConfig; 
