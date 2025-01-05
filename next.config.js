47a3b306d8e5e2fda2af8215f448910c40204b8d
Andrii Polishchuk <polishchuk91@gmail.com>
2025-01-05 10:02:54 +0200

    fixing static resources path

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
     },
-    assetPrefix: assetPrefix,
-    basePath: basePath,
 };
 
 module.exports = nextConfig; 
