33c35d83d8ef65c60b37ca8bd15d129ea4cd8722
Andrii Polishchuk <polishchuk91@gmail.com>
2025-01-05 16:46:24 +0200

    updated next config

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
     },
     basePath: '/master-react-course',
     assetPrefix: '/master-react-course/',
-    experimental: {
-        serverActions: false,
-    },
 };
 
 module.exports = nextConfig;
